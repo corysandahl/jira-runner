727c7826241adc6ea129654e483c4e03835957c1
Cory Sandahl <sandahl@wa-csandahl-mbp.local>
2019-05-04 16:33:53 -0700

    cleanup of unnecessary files that do not lend to the basic use of JiraRunner

--- confluence.js
+++
@@ -1,9 +0,0 @@
-var settings = require('./settings.js'),
-	Confluence = require('confluence-api'),
-	config = {
-	    username: settings.apiSettings.userName,
-	    password: settings.apiSettings.password,
-	    baseUrl:  settings.confluenceBaseURL
-	};
-
-module.exports = new Confluence(config);
